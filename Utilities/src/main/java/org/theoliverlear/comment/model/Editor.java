3732a8cacfeb87de99822d8a237690dbf9c96ded
theoliverlear <sigwartholiver2@gmail.com>
2023-10-17 14:14:43 -0500

    Add constructors

--- Utilities/src/main/java/org/theoliverlear/comment/model/Editor.java
+++ Utilities/src/main/java/org/theoliverlear/comment/model/Editor.java
@@ -3,9 +3,22 @@ package org.theoliverlear.comment.model;
 public class Editor {
     private int indentLevel;
     private int indentSize;
-    public Editor(int indentLevel, int indentSize) {
+    private int lineLength;
+    //----------------------------Constructors--------------------------------
+    public Editor() {
+        this.indentLevel = 0;
+        this.indentSize = 4;
+        this.lineLength = 78;
+    }
+    public Editor(int indentSize, int lineLength) {
+        this.indentLevel = 0;
+        this.indentSize = indentSize;
+        this.lineLength = lineLength;
+    }
+    public Editor(int indentLevel, int indentSize, int lineLength) {
         this.indentLevel = indentLevel;
         this.indentSize = indentSize;
+        this.lineLength = lineLength;
     }
     //------------------------------Getters-----------------------------------
     public int getIndentLevel() {
@@ -14,11 +27,17 @@ public class Editor {
     public int getIndentSize() {
         return this.indentSize;
     }
+    public int getLineLength() {
+        return this.lineLength;
+    }
     //------------------------------Setters-----------------------------------
     public void setIndentLevel(int indentLevel) {
         this.indentLevel = indentLevel;
     }
     public void setIndentSize(int indentSize) {
         this.indentSize = indentSize;
+    }
+    public void setLineLength(int lineLength) {
+        this.lineLength = lineLength;
     }
 }
